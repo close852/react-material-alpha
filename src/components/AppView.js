36be6b69d080f3fae517dbe27ca6657eddcdfe90
username <user@email.mail>
2019-10-21 07:40:05 +0900

    appView.js 통해서 결재문서 조회 하도록 필요함(기안,수정,조회 에 대한 판단)

---
+++ src/components/AppView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AppView({appId, formId}) {
+    return (
+        <div>
+            <div>버튼영역</div>
+            <div>본문영역 -  * 파일수정 수시로?(본문 생성 안하고, 매핑만 진행하는 방향으로 - 저장액션에서만 파일 생성 )</div>
+        </div>
+    )
+}
+
+export default AppView
